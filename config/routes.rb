c95c9b63b4cc36a8c92e30837cc409bac1ca4a18
KoshmareG <Koshmareg@yandex.ru>
2022-03-22 20:34:26 +0700

    Add new contacts view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :articles
+  resources :contacts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
