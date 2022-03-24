0c97d5b76d040cee568e680f585d94e881953832
KoshmareG <Koshmareg@yandex.ru>
2022-03-24 16:24:19 +0700

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,14 @@ end
 
 def create
     render 'create', status: :unprocessable_entity
+    @contact = Contact.new(contact_params)
+    @contact.save
+end
+
+private
+
+def contact_params
+    params.require(:contact).permit(:name, :email, :message)
 end
 
 end
