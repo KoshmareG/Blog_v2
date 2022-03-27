1d09bddc59f98618d2f8ba08079b5d1477e62503
KoshmareG <Koshmareg@yandex.ru>
2022-03-27 12:31:15 +0700

    Add rout and display article

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'about', action: :show, controller: 'about'
+  get '/articles/:id', to: 'articles#show', as: 'article'
 
-  resources :articles, only: [:index, :new, :create]
+  resources :articles
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resource :terms,  only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
