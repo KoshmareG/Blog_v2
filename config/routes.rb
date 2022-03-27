4f05100e739dd2908638354664f5c8df8e51ea08
KoshmareG <Koshmareg@yandex.ru>
2022-03-27 18:05:38 +0700

    Fix error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'about', action: :show, controller: 'about'
-  get '/articles/:id', to: 'articles#show', as: 'article'
 
   resources :articles
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
