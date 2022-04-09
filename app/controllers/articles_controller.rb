d1e1cbc0c73443cbc1e9585ff4f967b5f6bbe422
KoshmareG <Koshmareg@yandex.ru>
2022-04-09 17:18:59 +0700

    Chenge display articles on article#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @articles = Article.all
+        @articles = Article.order 'id desc'
     end
 
     def show
