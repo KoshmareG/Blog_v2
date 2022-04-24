1c2fc779cf4848718f47702e75a180e5ac534d12
KoshmareG <Koshmareg@yandex.ru>
2022-04-24 22:14:53 +0700

    Add user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   has_one :user
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validates :avatar, file_size: { less_than_or_equal_to: 10.megabytes },
+            file_content_type: { allow: ['image/jpeg', 'image/png', 'image/webp'] }
 end
