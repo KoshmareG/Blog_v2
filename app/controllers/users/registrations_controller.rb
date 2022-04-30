188352f2318aa936c4813e83e4d8664b906e54a8
KoshmareG <koshmareg@yandex.ru>
2022-04-30 17:43:02 +0700

    Update user information without password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,6 +39,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
@@ -47,7 +50,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar, :email, :password, :current_password, :password_confirmation)}
   end
 
   # The path used after sign up.
