5c92f97036531584f919806adacf56d019423ed6
KoshmareG <koshmareg@yandex.ru>
2022-05-03 19:21:23 +0700

    Add profiles_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_105413) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_03_120405) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -65,6 +65,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_105413) do
     t.string "name"
     t.text "email"
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
