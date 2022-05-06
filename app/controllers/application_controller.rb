46c518038cbbfb79c083e5983e323bfd06be401a
KoshmareG <koshmareg@yandex.ru>
2022-05-07 01:00:42 +0700

    Add russian lenguage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
+    I18n.default_locale = :ru
+
     private
 
     def configure_permitted_parameters
