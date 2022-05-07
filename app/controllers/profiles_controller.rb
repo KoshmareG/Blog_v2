2fad88ed837bc50bed54fde0cda2bda029502566
KoshmareG <koshmareg@yandex.ru>
2022-05-07 22:11:38 +0700

    Add paginate for articles list ob profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = Article.where(user_id: @user.id)
+        @articles = Article.where(user_id: @user.id).paginate(page: params[:page], per_page: 10)
     end
 
 end
