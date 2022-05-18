5b2a00a85a687d2523380175339edc484c17805d
KoshmareG <koshmareg@yandex.ru>
2022-05-18 21:13:50 +0700

    Update article spec

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-    it { should have_many :comments }
+    describe "validations" do
+        it { should validate_presence_of :title }
+        it { should validate_presence_of :text }
+    end
+    
+    describe "associations" do
+        it { should have_many :comments }
+    end
+
+    describe "#last_comment" do
+        it "return the last comment" do
+            article = create(:article_with_comments)
+            
+            expect(article.last_comment.body).to eq "comment body 3"
+        end
+    end
 end
