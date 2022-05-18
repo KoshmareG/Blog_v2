ca96026eeee41ff37da5984655c578502d52be4d
KoshmareG <koshmareg@yandex.ru>
2022-05-18 15:26:19 +0700

    Add comment model spec

---
+++ spec/model/comment_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Comment, type: :model do
+    it { should belong_to :article }
+end
+
+RSpec.describe Comment, type: :model do
+    it { should belong_to :user }
+end
