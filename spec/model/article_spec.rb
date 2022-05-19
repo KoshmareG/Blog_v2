646781fb8b33b1901ee49fbad37ab2cf34bd0b59
KoshmareG <koshmareg@yandex.ru>
2022-05-19 17:54:53 +0700

    Update article spec

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -2,13 +2,16 @@ require "rails_helper"
 
 RSpec.describe Article, type: :model do
     describe "validations" do
-        it { should validate_presence_of :title }
-        it { should validate_presence_of :text }
+        it { should validate_presence_of(:title) }
+        it { should validate_presence_of(:text) }
         it { should validate_length_of(:title).is_at_most(130) }
     end
     
     describe "associations" do
-        it { should have_many :comments }
+        it { should have_many(:comments) }
+        it { should have_many(:likes) }
+        it { should have_rich_text(:text) }
+        it { should belong_to(:user) }
     end
 
     describe "#last_comment" do
