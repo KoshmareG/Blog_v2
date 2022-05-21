95e8cf2a249e04ed26f3b0585da20d5e6a4a6fea
KoshmareG <koshmareg@yandex.ru>
2022-05-21 23:12:50 +0700

    Update test for article create

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -10,4 +10,23 @@ RSpec.feature "Article Creation", type: :feature do
 
         expect(page).to have_content('Новая статья')
     end
+
+    scenario "allows user to create and edit new article" do
+        visit(new_article_path)
+
+        fill_in :article_title, :with => 'Test article title'
+        find('trix-editor').set('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed')
+        click_button('Опубликовать')
+
+        expect(page).to have_content('Изменить')
+
+        find('#articleDropdown').click
+        click_on('Редактировать')
+
+        expect(page).to have_content('Lorem ipsum dolor')
+
+        click_button('Сохранить')
+
+        expect(page).to have_content('Изменить')
+    end
 end
