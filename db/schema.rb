70482a1a6f51ec66501531911696b6ee0edf0cf7
KoshmareG <koshmareg@yandex.ru>
2022-05-22 20:14:38 +0700

    Minor update

--- db/schema.rb
+++ db/schema.rb
@@ -60,17 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_084258) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string "data_file_name", null: false
-    t.string "data_content_type"
-    t.integer "data_file_size"
-    t.string "data_fingerprint"
-    t.string "type", limit: 30
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.index ["type"], name: "index_ckeditor_assets_on_type"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "body"
