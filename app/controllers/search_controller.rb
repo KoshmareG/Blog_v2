be1958bd7198b85c7226c7dbaf5edd2124866f55
KoshmareG <koshmareg@yandex.ru>
2022-06-07 18:43:18 +0700

    Improve search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,20 @@ class SearchController < ApplicationController
         if params[:request].blank?
             @result = nil
         else
-            search_array = params[:request].split(/ /)
+            request_array = params[:request].split(/ /)
+
+            additional_array = []
+
+            request_array.each do |word|
+                if word.size >= 4 && word.size <= 5
+                    additional_array << word[-1]
+                elsif word.size >= 6
+                    additional_array << word[-2]
+                end
+            end
+
+            search_array = request_array + additional_array
+
             search_request = search_array.map { |req| "%" + req + "%" }
             @results = Article.where("title ILIKE ANY ( array[?] )", search_request)
         end
